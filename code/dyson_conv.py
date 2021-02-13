269e0f5fe84c15ac75e1b62205b8f4ccdcc8e520
MLK97 <maximilianlukaskonrad@hotmail.de>
2021-02-13 14:59:09 +0100

    refined code and added potential plotting of G(E) v0.3

--- code/dyson_conv.py
+++ code/dyson_conv.py
@@ -2,11 +2,21 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import numpy as np
 
-rc('font', **{'family': 'serif',
-              'serif': ['Computer Modern'],
-              'size': 20})
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 20})
 rc('text', usetex=True)
 rc('figure', figsize=(11.69, 8.27))
+"""
+The main problem of this method
+is its very high instability
+when approximating G_11 via
+iteration and starting at g_11.
+
+Since G_11 appears as square,
+every value that is
+- higher than |0.5|
+- extremely small 10e-100
+causes very big fluctuations of the iteration
+"""
 
 
 """
@@ -14,7 +24,7 @@ The Dyson equation is of the form
 G = g + gVG,
 where G is a matrice.
 
-Eventually, for the components we arrive at
+Eventually, for the 11-component we arrive at
 G_11 = g_11 + g_11 t^2 G_11^2,
 
 where g_11 = 1/(E-epsilon) and
@@ -23,74 +33,117 @@ E = E +- i eta
 
 """
 Init Values:
-t is transfer matrice element and known
+t is transfer matrice element and assumed known
 g is the unperturbed Green function 
 """
 
-def approximate_G(t, g_11): 
-    for a in range(len(g_11)):
-        G[a][0] = g_11[a]
+"""
+This code is divided into 3 functions
 
-        print("t =", t, "\t", "energie =", g_11[a])
-        print("Step\tG[n-1]\tG[n]\tDifference")
+g_values(): creates the matrix containing g_11 values used in approximate_G()
+approximate_G(): approximates G_11 with the values t, g_11
+plot_G(): takes the best approximation of approximate_G() and plots them as G(E)
+
+plot_G()'s plot should be identical/similar to Fig 5.2 in Cuevas
+"""
+
+def approximate_G(t, g_11):
+    G = np.empty((13,40)) # change first value according to len(E)
+    x = np.arange(40)
+    g_11 = g_11.real
+    for m in range(len(g_11)):
+        if abs(g_11[m]) > 0.5:
+            """ checks if value is 0.5 and thus would diverge anyway"""
+            print("Initial Value =", g_11[m], " got skipped.")
+            continue
+        G[m][0] = g_11[m]
+
+        print("Current Value of m =", m)
+        print("Initial Value =", g_11[m])
+        print("Step\tG[n]")
 
         for n in x:
-            G[a][n] = g_11[a] + g_11[a] * t**2 * G[a][n-1]**2
-            diff[n] = np.abs(G[a][n-1] - G[a][n])
+            G[m][n] = g_11[m] + g_11[m] * t**2 * G[m][n-1]**2
+            print(n, "\t", G[m][n])
 
-            print(n, "\t", G[a][n-1], "\t", G[a][n], "\t", diff[n])
-    
-    # plt.plot(x, diff, marker="o", label="Green Convergence")
-    plt.plot(x, G[0], marker=".", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.1")
-    plt.plot(x, G[1], marker="v", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.2")
-    plt.plot(x, G[2], marker="s", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.4")
-    plt.plot(x, G[4], marker="x", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.5")
-    plt.plot(x, G[5], marker="D", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.6")
-    plt.plot(x, G[6], marker="p", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.7")
+    print(G[0])
+    #for l in range(0, len(g_11)-1):
+    plt.plot(x, G[0], marker="v", label=g_11[1])
 
-    plt.title("Numerical Evaluation of $G_{11}$ for fixed E")
+    #plt.ylim(-50000, 50000)
+    plt.grid(True)
+    #plt.legend()
+    plt.title("Approximation of $G_{11}$ by Iteration")
     plt.ylabel("$G_{11}$")
     plt.xlabel("Step")
-
-    plt.ylim(0, 2)
-    plt.grid(True)
-    plt.legend()
     plt.show()
     return G
 
 def plot_G(G, E):
     """
-    Let's assume we have a fixed epsilon of 
-    epsilon = 0.5
+    From approximate_G() we got
+    approximated values for G in (2,2) form
+    G[n][max-length] is the final approximated value
+    for the n-th variation of g_11.
+
+    approximate_G() currently varies g_11
+    by changing the Energy.
+    Thus we can plot G(E).
     """
-    end = len(G[0])
-    Green = np.empty(7)
-    for n in range(7):
-        Green[n] = G[n][end-1]
-    print(Green)
+    best_approx = len(G[0])-1
+    Green = np.empty(len(G))
+
+    for n in range(len(Green)-1):
+        print(G[n][best_approx])
+        Green[n] = G[n][best_approx]
+    
     plt.plot(E, Green, marker="p", label="$G_{11}$ @ 1/(E - $\epsilon$) = 0.7")
 
     plt.title("Numerical Evaluation of $G$ over E")
     plt.ylabel("G")
     plt.xlabel("E")
 
-    plt.ylim(0, 2)
+    # plt.ylim(0, 2)
     plt.grid(True)
-    plt.legend()
+    #plt.legend()
     plt.show()
 
-if __name__ == "__main__":
-    t = 1 # value taken from zilly
-    epsilon = [0.1, 0.2, 0.3, 0.4, 0.5] # epsilon = 0.5 in zilly
-    E = [-3, -2, -1, 0, 1, 2, 3] # range taken from zilly
-    g_11 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-    # hard coded atm for Energy = 0 change soon!
+def g_values(E):
+    """
+    g_11 is the unperturbed function of G_11
+    For the half infinite chain it is defined as
+    g_11 = 1/(E-epsilon) and
+    E = E +- i eta
+    """
 
-    G = np.empty((7,40))
+    """
+    Create Array of Energy values with
+    different etas
+    """
+    E_real = E
+    #E_imag = 1j*np.array([-0.1, -0.01, -0.001, 0.001, 0.01, 0.1])
+    E_imag = 1j*np.array([0.001])
+    Energy = np.array([0])
+    for n in E_real:
+        Energy = np.append(Energy, n + 1j * E_imag)
 
-    diff = np.empty(40)
-    diff[0] = 0
-    x = np.arange(40)
+    """
+    Compute g_11 with the formula
+    g_11 = 1/(E-epsilon).
+    At the moment epsilon is fixed and
+    epsilon = 0.5
+    """
+    epsilon = 0.5
+    g_11 = 1/(Energy-epsilon)
+    print(len(g_11))
+    return g_11
     
+if __name__ == "__main__":
+    t = 1 # Value used in Zilly
+    epsilon = [0.1, 0.2, 0.3, 0.4, 0.5] # Zilly uses 0.5  
+    E = np.array([-3, -2.5, -2, 1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 3])
+    
+    g_11 = g_values(E)
     G = approximate_G(t, g_11)
-    plot_G(G, E)
+    E = np.array([-3, -2, -1, 1, 2, 3])
+    #plot_G(G, E)
